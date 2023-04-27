83d9031523a9a52adb151dc8dba3d20f2dd250f6
Kurokami2022 <112530736+Kurokami2022@users.noreply.github.com>
2023-04-27 19:51:10 +0800

    Update: 04-27-2023

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,10 @@ import {
   IonItem,
   IonButton,
   IonIcon,
-  IonLabel
+  IonLabel,
+  IonImg,
+  IonButtons,
+  IonModal
 } from '@ionic/react';
 import './Home.css';
 import { 
@@ -17,7 +20,10 @@ import {
   barChartOutline,
   calendarOutline
 } from 'ionicons/icons';
+import { useState } from 'react';
 const Home: React.FC = () => {
+  const [isPurposeOpen, setIsPurposeOpen] = useState(false);
+  const [isFeaturesOpen, setIsFeaturesOpen] = useState(false);
   return (
     
     <IonPage>
@@ -28,6 +34,41 @@ const Home: React.FC = () => {
           <br />
           <IonLabel>AI Virtual Assistant</IonLabel>
           </IonList>
+
+          <IonButton onClick={() => setIsPurposeOpen(true)} className='btnpurp' fill="clear">Purpose</IonButton>
+          <IonModal isOpen={isPurposeOpen} className='modal1'>
+            <IonButtons slot="end">
+              <IonButton onClick={() => setIsPurposeOpen(false)}>Close</IonButton>
+            </IonButtons>
+            <IonContent>
+              <p className='purpcontent'>
+                <IonLabel className='purp'>PURPOSE</IonLabel><br /><br /><br />
+                <IonLabel className='stream'>Streamlining daily tasks and routines</IonLabel><br /><br />
+                <IonLabel className='enhance'>Enhancing productivity</IonLabel><br />
+                <IonLabel>Providing personalized recommendations</IonLabel><br />
+                <IonLabel>Providing financial guidance</IonLabel><br />
+                <IonLabel>Improving health and wellness</IonLabel>
+              </p>
+            </IonContent>
+          </IonModal>
+
+          <IonButton onClick={() => setIsFeaturesOpen(true)} className='btnfeat' fill="clear">Features</IonButton>
+          <IonModal isOpen={isFeaturesOpen} className='modal2'>
+            <IonButtons slot="end">
+              <IonButton onClick={() => setIsFeaturesOpen(false)}>Close</IonButton>
+            </IonButtons>
+            <IonContent>
+              <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos
+              reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui.
+              Eaque, dicta.
+              </p>
+            </IonContent>
+          </IonModal>
+
+          <IonImg src="/public/images/AIVA.png" alt="AI pic" className='aipic'/>
+          <IonImg src="/public/images/AIVA2.png" alt="AI pic" className='aipic2'/>
+          
           <IonList className='taBase'>
             <IonButton className='calendar' fill="clear">
               <IonIcon icon={calendarOutline} />
