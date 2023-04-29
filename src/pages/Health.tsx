3f6a107f0a9e8d5df4a1da30799114f11f9bd8d1
Kurokami2022 <112530736+Kurokami2022@users.noreply.github.com>
2023-04-29 23:00:13 +0800

    Update: 04-29-23/11:00pm

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -22,74 +22,22 @@ import {
   calendarOutline
 } from 'ionicons/icons';
 
-import { useRef, useState } from 'react';
-import * as tf from '@tensorflow/tfjs';
+import { useRef, useState, useEffect } from 'react';
+import natural from 'natural';
 
 const Health: React.FC = () => {
 
+
+
   const feelings = useRef<any>("");
-
-  const [recommendation, setRecommendation] = useState("");
-
-  const go = () => {
-    const input = feelings.current.value;
-    // load the pre-trained model
-    tf.loadLayersModel('/path/to/model.json').then(model => {
-      // tokenize the input text
-      const tokens = input.split(' ');
-      // preprocess the input text
-      const sequence = preprocess(tokens);
-      // make a prediction
-      const prediction = model.predict(sequence);
-      // decode the prediction into text
-      const text = decode(prediction);
-      // set the recommendation
-      setRecommendation(text);
-    });
-  }
-
-  // preprocess the input text
-  const preprocess = (tokens: string[]) => {
-    // convert the tokens into indices
-    const indices = tokens.map(token => {
-      // TODO: implement a mapping function to convert tokens to indices
-      return 0;
-    });
-    // pad the sequence to a fixed length
-    const maxLength = 50;
-    const paddedIndices = padSequence(indices, maxLength);
-    // convert the sequence into a tensor
-    const tensor = tf.tensor2d(paddedIndices, [1, maxLength]);
-    return tensor;
-  }
-
-  // pad the sequence to a fixed length
-  const padSequence = (sequence: number[], maxLength: number) => {
-    const padded = new Array(maxLength).fill(0);
-    const length = Math.min(sequence.length, maxLength);
-    for (let i = 0; i < length; i++) {
-      padded[i] = sequence[i];
-    }
-    return padded;
-  }
-
-  // decode the prediction into text
-  const decode = (prediction: tf.Tensor | tf.Tensor[]) => {
-    if (Array.isArray(prediction)) {
-      prediction = prediction[0];
-    }
-    // TODO: implement a decoding function to convert the prediction tensor to text
-    return "Recommendation";
-  }
-
   return (
     <IonPage>
       <IonContent fullscreen>
         <IonList className='base'>
           <IonInput placeholder='What do you feel' ref={feelings}></IonInput>
-          <IonButton onClick={go}>Go</IonButton>
+          <IonButton onClick={() => (feelings.current.value)}>Go</IonButton>
           <IonItem>
-            <IonLabel>{recommendation}</IonLabel>
+            <IonLabel>{}</IonLabel>
           </IonItem>
         </IonList>
       </IonContent>
